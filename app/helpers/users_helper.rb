364a7fe1005b919ba9f5e261ab97b6e6e4287a64
Wei Ma <weima.gm@gmail.com>
2019-12-24 20:36:08 -0800

    Added sign up page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,17 @@
 module UsersHelper
+  #Returns the Gravatar for the given user
+  def gravatar_for(
+      user,
+      options = {
+          size: 80
+      }
+  )
+    id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    url = "https://secure.gravatar.com/avatar/#{id}?s=#{size}"
+    image_tag(url, {
+        alt: user.name,
+        class: 'gravatar'
+    })
+  end
 end
