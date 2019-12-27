11969f30aa9e2200611b87e77d65045cee95a5b3
Wei Ma <weima.gm@gmail.com>
2019-12-26 20:49:48 -0800

    Finished user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
-    assert_no_difference 'User.count' do
+    assert_no_difference('User.count') do
       post(
           users_path,
           {
@@ -20,5 +20,30 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
     end
     assert_template 'users/new'
+    assert_select('div#error_explanation')
+    assert_select('div.field_with_errors')
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference('User.count', 1) do
+      post(
+          users_path,
+          {
+              params: {
+                  user: {
+                      name: "Example User",
+                      email: "user@example.com",
+                      password: "passpass",
+                      password_confirmation: "passpass"
+                  }
+              }
+          }
+      )
+
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not(flash[:error])
   end
 end
